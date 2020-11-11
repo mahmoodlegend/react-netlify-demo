910b75873e3d7b98019c24466fecce176b2348b8
mohammadmahmood441@gmail.com <m.mahmood8176@gmail.com>
2020-11-11 10:49:05 +0500

    react tutorial through git

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     Hello From React!! New !!
+     Hello From React!! New !! Through Git CI $ CD Pipelines
     </div>
   );
 }
